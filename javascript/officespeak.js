9ebe55018334c5cf4a0a433a58329340622d38a4
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-07-25 16:03:48 -0500

    adding placeholder code for for each loop

---
+++ javascript/officespeak.js
@@ -0,0 +1,40 @@
+console.log ("officespeak.js");
+
+let finalOutput = [];
+
+function translateToOfficeSpeak(individualWords) {
+	forEach(item in individualWords) {
+		if (item == myOfficeSpeak.key) {
+			item = myOfficeSpeak.value;
+			push.finalOutput(item);
+		}
+	}
+}
+
+
+let outputArea = getElementById("");
+
+var Knights = (function(originalKnights) {
+
+let myOfficeSpeak = {
+
+    "merry ":"Leverage agile frameworks ", 
+    "christmas ":"robust synopsis ",
+    "ya ":"overall value proposition ",
+    "filthy ":"organically grow ",
+    "animal ":"generation X ",
+    "hanukkah ":"streamlined cloud solution ",
+    "kwanzaa ":"ballpark value ",
+    "tree ":"ROI ",
+    "good ":"offshoring ",
+    "tidings ":"real-time ", 
+    "and ":"nanotechnology ", 
+    "happy ":"podcasting ", 
+    "new ":"information highway ", 
+    "year ":"digital divide"
+
+}
+
+
+	return originalKnights;
+})(Knights);
