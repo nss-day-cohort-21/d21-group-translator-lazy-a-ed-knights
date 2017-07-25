cce872f2571cb1d9883318b5365d711740d9d1eb
Matt Minner <matthewfminner@gmail.com>
2017-07-25 15:08:34 -0500

    finished button interactivity

---
+++ javascript/Dothraki.js
@@ -0,0 +1,22 @@
+console.log("Dothraki.js");
+
+let outputArea = getElementById("");
+
+var Knights = (function(originalKnights){
+   let Dothraki = {
+    "merry ":"", 
+    "christmas ":"",
+    "ya ": "",
+    "filthy ": "",
+    "animal ": "",
+    "hanukkah ":"",
+    "kwanzaa ":"",
+    "tree ":"",
+    "good ":"",
+    "tidings ":"", 
+    "and ":"", 
+    "happy ":"", 
+    "new ":"", 
+    "year ":"",
+    }
+});
