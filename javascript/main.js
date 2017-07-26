e176188c9fa99eac80c278ff46b983ea8ea17f42
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-07-26 09:06:33 -0500

    add for merge

--- javascript/main.js
+++ javascript/main.js
@@ -2,44 +2,14 @@ console.log("main.js");
 
 let addTranslate = document.getElementById("btn-translate");
 
-addTranslate.addEventListener("click", () => {
-    let textTrans = document.getElementById("transText").value;
-    var individualWords = textTrans.split(" ");
-    for (var i = 0; i < individualWords.length - 1; i++) {
-            individualWords[i] += " ";
-     console.log("individualWords", individualWords);
-}
-
-    let whichselected;
-    var radios = document.getElementsByName("rad-lang");
-    for(let i = 0; i < radios.length; i++){
-        //0 = Binary, 1 = Dothraki, 2 = Italian, 3 = Office Speak
-        if (radios[i].checked){
-            whichselected = radios[i].value;
-            break;
-        }
-    }
-    console.log ('whichselected', whichselected);
-
-    if (whichselected == 0){
-        Knights.translateToBinary(individualWords);
-    }else if (whichselected == 1){
-        Knights.tranlsateToDothraki(individualWords);
-    }else if (whichselected == 2){
-        Knights.translateToItalian(individualWords);
-    }else if (whichselected ==3){
-        Knights.translateToOfficeSpeak(individualWords);
-    }
-});
-
-
-//THIS IS THE CODE THAT WORKS FOR ALL MVP
-// let transText = document.getElementById("transText");
-// let addTranslate = document.getElementById("btn-translate");
-// let output = document.getElementById("transUpdated");
-
-
 // addTranslate.addEventListener("click", () => {
+//     let textTrans = document.getElementById("transText").value;
+//     var individualWords = textTrans.split(" ");
+//     for (var i = 0; i < individualWords.length - 1; i++) {
+//             individualWords[i] += " ";
+//      console.log("individualWords", individualWords);
+// }
+
 //     let whichselected;
 //     var radios = document.getElementsByName("rad-lang");
 //     for(let i = 0; i < radios.length; i++){
@@ -52,33 +22,63 @@ addTranslate.addEventListener("click", () => {
 //     console.log ('whichselected', whichselected);
 
 //     if (whichselected == 0){
-//         Knights.translateToBinary()
+//         Knights.translateToBinary(individualWords);
 //     }else if (whichselected == 1){
-//        Knights.tranlsateToDothraki()
-//     }
-//     else if (whichselected == 2){
-//         Knights.translateToItalian();
+//         Knights.tranlsateToDothraki(individualWords);
+//     }else if (whichselected == 2){
+//         Knights.translateToItalian(individualWords);
 //     }else if (whichselected ==3){
-//         Knights.translateToOfficeSpeak();
+//         Knights.translateToOfficeSpeak(individualWords);
 //     }
 // });
 
-// // function translateToItalian(event){
-// //  Knights.translateItToItalian();
 
-// // };
+// THIS IS THE CODE THAT WORKS FOR ALL MVP
+let transText = document.getElementById("transText");
+let addTranslate = document.getElementById("btn-translate");
+let output = document.getElementById("transUpdated");
 
-// // function translateToBinary(){
-// //  Knights.translateItToBinary();
-// // };
 
-// // function tranlsateToDothraki(){
-// //  Knights.translateItToDothraki();
-// // };
+addTranslate.addEventListener("click", () => {
+    let whichselected;
+    var radios = document.getElementsByName("rad-lang");
+    for(let i = 0; i < radios.length; i++){
+        //0 = Binary, 1 = Dothraki, 2 = Italian, 3 = Office Speak
+        if (radios[i].checked){
+            whichselected = radios[i].value;
+            break;
+        }
+    }
+    console.log ('whichselected', whichselected);
 
-// // function translateToOfficeSpeak(){
-// //  Knights.translateItToOfficeSpeak();
-// // };
+    if (whichselected == 0){
+        Knights.translateToBinary()
+    }else if (whichselected == 1){
+       Knights.tranlsateToDothraki()
+    }
+    else if (whichselected == 2){
+        Knights.translateToItalian();
+    }else if (whichselected == 3){
+        Knights.translateToOfficeSpeak();
+    }
+});
+
+function translateToItalian(event){
+ Knights.translateItToItalian();
+
+};
+
+function translateToBinary(){
+ Knights.translateItToBinary();
+};
+
+function tranlsateToDothraki(){
+ Knights.translateItToDothraki();
+};
+
+function translateToOfficeSpeak(){
+ Knights.translateItToOfficeSpeak();
+};
 
 
 
