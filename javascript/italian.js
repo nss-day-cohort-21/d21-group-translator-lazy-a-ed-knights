a4aaa07847d3d059820e42a7400086cd707d771c
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-07-25 20:17:27 -0500

    updates to IIFE files and created language functions to call said files

--- javascript/italian.js
+++ javascript/italian.js
@@ -1,25 +1,39 @@
 console.log("italian.js, yo!");
 
+var Knights = (function(originalKnights){
 
-var Knights = (function(){
+	let italian =  { 
+			merry:"MERRY", 
+		    christmas:"CHRISTMAS",
+		    ya: "YA",
+		    filthy: "FILTHY",
+		    animal: "ANIMAL",
+		    hanukkah:"HANNNNNNNN",
 
-let italian =  { 
-		merry:"", 
-	    christmas:"",
-	    ya: "",
-	    filthy: "",
-	    animal: "",
-	    hanukkah:"",
-	    kwanzaa:"",
-	    tree:"",
-	    good:"",
-	    tidings:"", 
-	    and:"", 
-	    happy:"", 
-	    new:"", 
-	    year:"",
-	    !:"",
-	    .:""
+	}
 
-	},
+	originalKnights.translateItToItalian = function(){
+		let userInput = transText.value;
+	let newUserInput = userInput.split(" ");
+	let transArray = [];
+	
+	newUserInput.forEach(function(word){
+		// console.log(Object.keys(italian));
+		// console.log(italian[word]);
+		let translation = italian[word];
+		if (word in italian){
+		transArray.push(translation)
+		} else{
+			console.log(word, "has been removed because it is not in our dictionary.");
+			document.getElementById("error-message").innerHTML = `"${word}" has been removed because it is not in our dictionary.`
+		}
+	});
+
+		let transJoin = transArray.join(" ");
+		console.log("transJoin", transJoin);
+		document.getElementById("transUpdated").innerHTML = `${transJoin}`
+	}
+
+	return originalKnights;
+
 })(Knights);
